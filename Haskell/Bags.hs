0713a9060abd785c862228e638b6ddf36494cccc
Dhruvisha <dhruvishasupeda@gmail.com>
2018-10-02 20:46:38 +0100

    I tried

--- Haskell/Bags.hs
+++ Haskell/Bags.hs
@@ -3,10 +3,20 @@ Bags.hs
 Assignment hell hi Phil
 **********************-}
 module Bags where
-  type Bag = List
-  listToBag::
-  bagEqual::
-  bagInsert::
-  bagSum::
+  type Bag = ([String], [Int])
+  
+ --Create empty bag
+  bcreate::Bag
+  bcreate=()
+
+  --listToBag::Googlenit? Idk
+
+  --bagEqual::Maybe use Eq?? Maybe ==??
+
+  --bagInsert::bag lol who knows
+    
+  --bagSum:: DEFFO lol who knows
+
+  --bagIntersection:: he skipped it in the lecture sooo
   bagIntersection::
     
