75c7838517b829d3dbcfcef5a8978abc28f405ae
Dhruvisha <dhruvishasupeda@gmail.com>
2018-10-05 13:42:25 +0100

    Can insert but it doesn't saveffs

--- Haskell/Bags.hs
+++ Haskell/Bags.hs
@@ -9,13 +9,28 @@ module Bags where
   bcreate::Bag
   bcreate=[]
 
+  connasse::Bag
+  connasse=[("hello", 7),("merde",1)]
+
   --listToBag::Googlenit? Idk
---  listToBag
+  --listToBag::[String]
+  --so basically, go to thehead and if it exists, add one to the number
+  --if it doesn't, make a new tuple with value of 1
+
+  --go through whole list,conitnously using bag insert until you're
+  --driven to jump off the arts tower
 
   --bagEqual::Maybe use Eq?? Maybe ==??
 
-  bagInsert::String->Int->Bag->Bag
-  bagInsert p number bag = ((p, number):bdelete p bag)
+  bPut::String->Int->Bag->Bag
+  bPut p number bag = ((p, number):bdelete p bag)
+  --find it using bget
+  --if it exists, add one to the number
+  --if not, make a new tuple
+  bagInsert::String->Bag->Bag
+  bagInsert p bag
+     |existsbitch p bag  = bPut p (bitchesbeincrementing p bag) bag
+     |otherwise = bPut p 1 bag
 
   --bagSum:: DEFFO lol who knows
 
@@ -28,9 +43,19 @@ module Bags where
     |otherwise = ((q,number):bdelete p rbag)
     where ((q,number):rbag)=bag
 
-  bget::String->Bag->String
+  bget::String->Bag->Int
   bget q bag
     |null bag=error"item not present"
-    |p==q = q
+    |p==q = number
     |otherwise = bget q rbag
     where ((p,number):rbag) = bag
+
+  existsbitch::String->Bag->Bool
+  existsbitch stringgggggggg bag
+    |null bag=False
+    |p==stringgggggggg = True
+    |otherwise = existsbitch stringgggggggg rbag
+    where ((p,number):rbag) = bag
+
+  bitchesbeincrementing::String->Bag->Int
+  bitchesbeincrementing fuckoff bag = (bget fuckoff bag) + 1
