c91556c0860af430d24da841321a9cbae45f26f6
Dhruvisha <dhruvishasupeda@gmail.com>
2018-10-05 11:40:01 +0100

    Delete kinda works

--- Haskell/Bags.hs
+++ Haskell/Bags.hs
@@ -3,20 +3,33 @@ Bags.hs
 Assignment hell hi Phil
 **********************-}
 module Bags where
-  type Bag = ([String], [Int])
-  
+  type Bag = [(String, Int)]
+
  --Create empty bag
   bcreate::Bag
-  bcreate=()
+  bcreate=[]
 
   --listToBag::Googlenit? Idk
 
   --bagEqual::Maybe use Eq?? Maybe ==??
 
-  --bagInsert::bag lol who knows
-    
+  bagInsert::String->String->Bag->Bag
+  bagInsert p number bag = ((p, number):bdelete p bag)
+
   --bagSum:: DEFFO lol who knows
 
   --bagIntersection:: he skipped it in the lecture sooo
-  bagIntersection::
-    
+
+  bdelete::String->Bag->Bag
+  bdelete p bag
+    |null bag = []
+    |p==q = rbag
+    |otherwise = ((q,number):bdelete p rbag)
+  where ((q,number):rbag)=bag
+
+  bget::String->Bag->String
+  bget q bag
+    |null pl=error"item not present"
+    |p==q = rpl
+    |otherwise = bget q rbag
+   where ((p,number):rbag)
