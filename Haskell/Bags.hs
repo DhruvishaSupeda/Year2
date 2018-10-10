e82dd4d803c15ca3c0249a40c2148d1504ed4101
Dhruvisha <dhruvishasupeda@gmail.com>
2018-10-10 19:34:44 +0100

    Made it semi PG

--- Haskell/Bags.hs
+++ Haskell/Bags.hs
@@ -18,35 +18,34 @@ module Bags where
   wtaf::Bag String
   wtaf=[("WHY",4)]
 
-  listToBag::Eq a =>[a]->Bag a
+  listToBag::Eq a => [a] -> Bag a
   listToBag list1 = listToBagA list1 bcreate
 
-  listToBagA::Eq a =>[a]->Bag a->Bag a
+  listToBagA::Eq a => [a] -> Bag a -> Bag a
   listToBagA list1 bag
     |null list1 = bag --if the string list is empty
     |otherwise = listToBagA t (bagInsert h bag) --inserts the iteminto the bag then recurses
     where (h:t) = list1
 
-  bagEqual::Eq a =>Bag a->Bag a->Bool
+  bagEqual::Eq a => Bag a -> Bag a -> Bool
   bagEqual bag1 bag2
     |null bag1 && null bag2 = True --if both bags empty,technically equal
     |null bag1 || null bag2 = False --if only one is empty, false
-    |itemExists (fst h1) bag2 && snd h1 == bget (fst h1) bag2 =
+    |(itemExists (fst h1) bag2) && (snd h1 == bget (fst h1) bag2) =
       bagEqual (bdelete (fst h1) bag1) (bdelete (fst h1) bag2)
       --if it exists in the second bag, then recurse with deleted from both bags
     |otherwise = False
     where (h1:t1) = bag1
-          (h2:t2) = bag2
 
-  bPut::Eq a =>a->Int->Bag a->Bag a
-  bPut p number bag = ((p, number):bdelete p bag)
+  bPut::Eq a => a -> Int -> Bag a -> Bag a
+  bPut item number bag = ((item, number):bdelete item bag)
 
-  bagInsert::Eq a =>a->Bag a->Bag a
-  bagInsert p bag
-    |itemExists p bag = bPut p (numberIncrement p bag) bag
-    |otherwise = bPut p 1 bag
+  bagInsert::Eq a => a -> Bag a -> Bag a
+  bagInsert item bag
+    |itemExists item bag = bPut item (numberIncrement item bag) bag
+    |otherwise = bPut item 1 bag
 
-  bagSum::Eq a =>Bag a->Bag a->Bag a
+  bagSum::Eq a => Bag a -> Bag a -> Bag a
   bagSum bag1 bag2
     |null bag1 && null bag2 = bcreate
     |null bag2 = bag1
@@ -61,48 +60,47 @@ module Bags where
     --if it exists, insert items in second and first into new bag andrecurse
     --if it doesn't, add and recurse
 
-  bagIntersection::Eq a=>Bag a->Bag a->Bag a
+  bagIntersection::Eq a=> Bag a -> Bag a -> Bag a
   bagIntersection bag1 bag2 = bagIntersectionA bag1 bag2 bcreate
 
-  bagIntersectionA::Eq a=>Bag a->Bag a->Bag a->Bag a
-  bagIntersectionA bag1 bag2 bagNew
-    |null bag1 = bagNew
+  bagIntersectionA::Eq a=> Bag a -> Bag a -> Bag a -> Bag a
+  bagIntersectionA bag1 bag2 bagnew
+    |null bag1 = bagnew
     |null bag1 || null bag2 = bcreate
     |itemExists (fst h1) bag2 =
-      bagIntersectionA (bdelete (fst h1) bag1) bag2 (bPut (fst h1) (smallestValue (snd h1) (bget (fst h1) bag2))bagNew)
-    |otherwise = bagIntersectionA (bdelete (fst h1) bag1) bag2 bagNew
+      bagIntersectionA (bdelete (fst h1) bag1){----} bag2 {----}(bPut (fst h1) (smallestValue (snd h1) (bget (fst h1) bag2))bagnew)
+    |otherwise = bagIntersectionA (bdelete (fst h1) bag1) bag2 bagnew
     where (h1:t1) = bag1
-          (h2:t2) = bag2
 
   --if it exitst and no1<no2, keep and recurse, using t1
   --if it exists and no2<no1,replace with no2 and recurse, using t1
   --if it doesn't exist, delete and recurse--}
 
-  bdelete::Eq a => a->Bag a->Bag a
-  bdelete p bag
+  bdelete::Eq a => a -> Bag a -> Bag a
+  bdelete item bag
     |null bag = []
-    |p==q = rbag
-    |otherwise = ((q,number):bdelete p rbag)
+    |item==q = rbag
+    |otherwise = ((q,number):bdelete item rbag)
     where ((q,number):rbag)=bag
 
-  bget::Eq a=> a->Bag a->Int
+  bget::Eq a => a -> Bag a -> Int
   bget q bag
     |null bag=error"item not present"
     |p==q = number
     |otherwise = bget q rbag
     where ((p,number):rbag) = bag
 
-  itemExists::Eq a => a->Bag a->Bool
+  itemExists::Eq a => a-> Bag a -> Bool
   itemExists item bag
     |null bag=False
     |p==item = True
     |otherwise = itemExists item rbag
     where ((p,number):rbag) = bag
 
-  numberIncrement::Eq a=>a->Bag a->Int
+  numberIncrement::Eq a => a -> Bag a -> Int
   numberIncrement fuckoff bag = (bget fuckoff bag) + 1
 
-  smallestValue::Int->Int->Int
+  smallestValue::Int -> Int -> Int
   smallestValue int1 int2
     |int1>int2 = int2
     |int2>int1 = int1
