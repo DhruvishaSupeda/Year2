e89d14a94c2ea21a466c7ef11e4babd4a124b26c
Dhruvisha <dhruvishasupeda@gmail.com>
2018-11-06 18:25:09 +0000

    Work in progress

--- Haskell/SolitaireOne.hs
+++ Haskell/SolitaireOne.hs
@@ -16,42 +16,14 @@ module SolitaireOne where
   type Reserves = Deck
   type EOBoard = (Foundations, Columns, Reserves) --god knows
 
-  new::[[Int]]
-  new=sequence [[1,2,3], [7,27,37]]
-
-{-- --Create empty bag
-  bcreate::Bag a
-  bcreate=[]
-
-  --Calls the auxiliary function listToBagA with an empty bag
-  listToBag::Eq a => [a] -> Bag a
-  listToBag list1 = listToBagA list1 bcreate
-
-  listToBagA::Eq a => [a] -> Bag a -> Bag a
-  listToBagA list bag
-   --once/if the list is empty, returns the bag
-    |null list = bag
-   --inserts the item into the bag then recurses
-    |otherwise = listToBagA t (bagInsert h bag) --inserts the item into the bag then recurses
-    where (h:t) = list
-
-  bagEqual::Eq a => Bag a -> Bag a -> Bool
-  bagEqual bag1 bag2 --}
-
 --------------------------------------------------------------------------------------------------------
   --for returning predecessor, use succ and pred of the enum types
   --for the Pip so you just find the value, then add the suit back
   sCard::Card -> Card
-  sCard start
-    |isKing start = start --is this correct???
-    |otherwise = (succ pip,suit)
-    where (pip,suit) = start
+  sCard card = (succ (fst card),(snd card))
 
   pCard::Card -> Card
-  pCard start
-    |isAce start = start --idk if this is right though, like is that the pred???
-    |otherwise = (pred pip,suit)
-    where (pip,suit) = start
+  pCard card = (pred (fst card),(snd card))
 
   isAce::Card -> Bool
   isAce card = fst card == Ace
@@ -77,6 +49,13 @@ module SolitaireOne where
   getInts = take 52 (randoms (mkStdGen 42)::[Int])
 
   eODeal::EOBoard
-  eODeal = ([],chunksOf 6 (drop 4 shuffle),(take 4 shuffle))
-  --eODeal = drop 4 shuffle
-  --(Foundations, [Columns], Reserves)
+  eODeal = ([], chunksOf 6 (drop 4 shuffle), (take 4 shuffle))
+
+  getColumns::[Int] -> [Int]
+  getColumns board = [2*n|n<-board,n>3]
+
+  getColumnHeads::EOBoard -> Card
+  getColumnHeads board = head (head c)
+    where (f,c,r) = board
+  --toFoundations::EOBoard -> EOBoard
+  --toFoundations startBoard = [2*n|n<-list,n==succ item]
